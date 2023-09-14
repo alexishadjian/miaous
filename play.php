16a6564cd6e494d74ba0935d8800c95076cbb330
Alexis <alexishadjian@gmail.com>
2023-09-14 11:36:04 +0200

    Score infos design

--- play.php
+++ play.php
@@ -1,13 +1,19 @@
 <?php require_once 'components/header.php'; ?>
 <div id="gameover"><p>Game Over</p></div>
 <div id="play">
-    <div id="map"></div>
 
-    <div class="bottom-infos wrapper">
-        <p>Distance : <span class="distance">0</span> mètres</p>
-        <p>Score : <span class="score">0</span> points</p>
-        <p>Temps restant : <span id="timer"></span></p>
+    <div class="map-container">
+    <div id="map"></div>
+        <div class="top-infos wrapper">
+            <div class="score-container">
+                <img src="assets/img/etoile.png" alt="">
+                <span class="score">0</span>
+            </div>
+            <p>Distance : <span class="distance">0</span> mètres</p>
+            <p>Temps restant : <span id="timer"></span></p>
+        </div>
     </div>
+
 </div>
 
 <?php require_once 'components/footer.php'; ?>
