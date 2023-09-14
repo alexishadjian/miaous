0849e085ccf51e762a6e05e7be3171aadec094aa
Mahylan <mahylan.veclin@wangarden.com>
2023-09-14 10:13:11 +0200

    game over

--- play.php
+++ play.php
@@ -1,11 +1,12 @@
 <?php require_once 'components/header.php'; ?>
-
+<div id="gameover"><p>Game Over</p></div>
 <div id="play">
     <div id="map"></div>
 
     <div class="bottom-infos wrapper">
         <p>Distance : <span class="distance">0</span> mètres</p>
         <p>Score : <span class="score">0</span> points</p>
+        <p>Temps restant : <span id="timer"></span></p>
     </div>
 </div>
 
--- script.js
+++ script.js
@@ -122,3 +122,25 @@ if ( mapTag ) {
         alert("La géolocalisation n'est pas disponible dans votre navigateur.");
     }
 }
+
+//timer
+//timer
+const departMinutes = 5
+let temps = departMinutes * 60
+
+const timerElement = document.getElementById("timer")
+
+setInterval(() => {
+    let minutes = parseInt(temps / 60, 10)
+    let secondes = parseInt(temps % 60, 10)
+
+    minutes = minutes < 10 ? "0" + minutes : minutes
+    secondes = secondes < 10 ? "0" + secondes : secondes
+
+    timerElement.innerText = `${minutes}:${secondes}`
+    temps = temps <= 0 ? 0 : temps - 1
+    if (temps + 2 === 0) {
+        document.getElementById("gameover").style.display = "block"
+    }
+
+}, 1000)
