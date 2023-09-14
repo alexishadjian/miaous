111c5c1cc7c84c1b5c905cab4ea05920f3e353af
Alexis <alexishadjian@gmail.com>
2023-09-14 10:37:34 +0200

    Rules

--- rules.php
+++ rules.php
@@ -7,6 +7,44 @@
     </nav>
     <h1>Les règles</h1>
     
+    <h3>Carte Interactive :</h3>
+
+    <p>Explorez la carte en temps réel.
+    Repérez les icônes de souris pour localiser les cibles.</p>
+
+    <h3>Chasse aux Souris :</h3>
+
+    <p>Déplacez-vous dans la vraie vie pour vous rendre aux emplacements des souris.
+    Soyez rapide, car les souris peuvent apparaître n'importe où !</p>
+    
+    <h3>Attraper une Souris :</h3>
+
+    <p>Approchez-vous suffisamment d'une souris pour l'attraper.
+    Une autre souris apparaitra alors !</p>
+    
+    <h3>Gestion du Temps :</h3>
+
+    <p>Vous avez un temps limité (ex. 3 minutes) pour attraper la prochaine souris et ainsi de suite.</p>
+    
+    <h3>Calcul du Score :</h3>
+
+    <p>Chaque souris attrapée rapporte des points (ex. 10 points par souris).
+    Votre score total est affiché à la fin du jeu.</p>
+    
+    <h3>Classement :</h3>
+
+    <p>Consultez le classement en ligne pour voir comment vous vous comparez aux autres joueurs.</p>
+    
+    <h3>Récompenses :</h3>
+
+    <p>Gagnez des médailles et des récompenses spéciales pour des performances exceptionnelles.</p>
+    
+    <h3>Sécurité :</h3>
+
+    <p>Soyez prudent lors de vos déplacements dans la vie réelle.
+    Respectez les règles de sécurité et la législation en vigueur.</p>
+    
+    <h4>À vous de jouer ! Attrapez ces souris virtuelles et montrez votre talent de chasseur de souris. Bonne chance !</h4>
 </div>
 
 <?php require_once 'components/footer.php'; ?>
