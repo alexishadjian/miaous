52156d7f246ce2e7c27c3881d05c8bef78e281ca
Alexis <alexishadjian@gmail.com>
2023-09-15 10:43:34 +0200

    undo last commit

--- components/header.php
+++ components/header.php
@@ -4,14 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="css/style.css" rel="stylesheet">
-    <!-- <link href="assets/leaflet/leaflet.css" rel="stylesheet"> -->
-    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/miaouss.png"> <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-     integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-     crossorigin=""/>
-     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-     integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-     crossorigin=""></script>
-    <!-- <script defer src="assets/leaflet/leaflet.js"></script> -->
+    <link href="assets/leaflet/leaflet.css" rel="stylesheet">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/miaouss.png">
+    <script defer src="assets/leaflet/leaflet.js"></script>
     <script defer src="script.js"></script>
     <title>Miaouss</title>
 </head>
